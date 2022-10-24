7ef03cf8aafda545146e0364909897b434d7ae79
Abby F <abby_faigao@yahoo.com>
2022-10-23 18:18:55 -0700

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Home } from "./pages/Home";
-import { Store } from "./pages/Store";
-import { About } from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 function App() {
   return (
     <Container className="mb-4">
-      Hello
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
